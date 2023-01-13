60fbf5a84b10ce3c0dd069703a06837190619224
Sviataslau Apanasionak <s.apanasionak@setplex.com>
2023-01-13 18:27:37 +0300

    commit

---
+++ src/main/java/com/example/lazyhang/SpringJpaLazyHangApplication.java
@@ -0,0 +1,12 @@
+package com.example.lazyhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaLazyHangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaLazyHangApplication.class, args);
+    }
+}
